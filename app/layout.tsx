7256d9a4196e7c90b170f43f979ca26f525fdd57
ismail ayhan <139040563+isometax@users.noreply.github.com>
2026-01-07 22:49:49 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,10 @@
-import "./globals.css";
-
 export const metadata = {
   title: "Retrobase Frame",
   description: "Mint your retro pixel NFT on Base",
   other: {
     "fc:frame": "true",
-    "fc:frame:image":
-      "https://retrobase-cards.nfts2.me/api/image/1042837497527936355385278742332698144974027353552",
+    "fc:frame:image": "https://retrobase-frame.vercel.app/nft.png",
     "fc:frame:button:1": "Mint",
     "fc:frame:post_url": "https://retrobase-frame.vercel.app/api/mint",
   },
 };
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
